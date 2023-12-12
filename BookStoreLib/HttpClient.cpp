8723b3562e5799552f4c5cfca431dab0f8fab3b1
Vitor Matosinho Martins <vitor_m_martins@yahoo.com.br>
2023-12-12 19:24:25 +0000

    feat: CMakeLists.txt updated

--- BookStoreLib/HttpClient.cpp
+++ BookStoreLib/HttpClient.cpp
@@ -1,6 +1,6 @@
 // HttpClient.cpp
 #include "HttpClient.h"
-#include <curl/curl.h>
+#include "../cmake-build-debug/_deps/httplib-src/httplib.h"
 
 char* HttpClient::sendHttpGet() {
     httplib::Client cli(googleapisURL);
